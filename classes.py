781c0b9aa55a8390e0eb8c0ee3b43c2223827c9d
Coookie <timohuhnholz@gmail.com>
2019-12-23 13:24:41 +0100

    Adding ability to add and remove names from the list inside the program

--- classes.py
+++ classes.py
@@ -2,3 +2,159 @@ class Player:
     def __init__(self, name):
         self.name = name
         self.battles = 0
+
+
+def getnames():
+    player1 = Player("")
+    player2 = Player("")
+    player3 = Player("")
+    player4 = Player("")
+    player5 = Player("")
+    player6 = Player("")
+    player7 = Player("")
+    player8 = Player("")
+    player9 = Player("")
+    player10 = Player("")
+    player11 = Player("")
+    player12 = Player("")
+    player13 = Player("")
+    player14 = Player("")
+    player15 = Player("")
+    player16 = Player("")
+    player17 = Player("")
+    player18 = Player("")
+    player19 = Player("")
+    player20 = Player("")
+    player21 = Player("")
+    player22 = Player("")
+    player23 = Player("")
+    player24 = Player("")
+    player25 = Player("")
+    player26 = Player("")
+    player27 = Player("")
+    player28 = Player("")
+    player29 = Player("")
+    player30 = Player("")
+    player31 = Player("")
+    player32 = Player("")
+    player33 = Player("")
+    player34 = Player("")
+    player35 = Player("")
+    player36 = Player("")
+    player37 = Player("")
+    player38 = Player("")
+    player39 = Player("")
+    player40 = Player("")
+    player41 = Player("")
+    player42 = Player("")
+    player43 = Player("")
+    player44 = Player("")
+    player45 = Player("")
+    player46 = Player("")
+    player47 = Player("")
+    player48 = Player("")
+    player49 = Player("")
+    player50 = Player("")
+    player51 = Player("")
+    player52 = Player("")
+    player53 = Player("")
+    player54 = Player("")
+    player55 = Player("")
+    player56 = Player("")
+    player57 = Player("")
+    player58 = Player("")
+    player59 = Player("")
+    player60 = Player("")
+    player61 = Player("")
+    player62 = Player("")
+    player63 = Player("")
+    player64 = Player("")
+    player65 = Player("")
+    player66 = Player("")
+    player67 = Player("")
+    player68 = Player("")
+    player69 = Player("")
+    player70 = Player("")
+    player71 = Player("")
+    player72 = Player("")
+    player73 = Player("")
+    player74 = Player("")
+    player75 = Player("")
+    player76 = Player("")
+    player77 = Player("")
+    player78 = Player("")
+    player79 = Player("")
+    player80 = Player("")
+    player81 = Player("")
+    player82 = Player("")
+    player83 = Player("")
+    player84 = Player("")
+    player85 = Player("")
+    player86 = Player("")
+    player87 = Player("")
+    player88 = Player("")
+    player89 = Player("")
+    player90 = Player("")
+    player91 = Player("")
+    player92 = Player("")
+    player93 = Player("")
+    player94 = Player("")
+    player95 = Player("")
+    player96 = Player("")
+    player97 = Player("")
+    player98 = Player("")
+    player99 = Player("")
+    player100 = Player("")
+
+    players = (player1, player2, player3, player4, player5, player6, player7, player8, player9, player10, player11,
+               player12, player13, player14, player15, player16, player17, player18, player19, player20, player21,
+               player22,
+               player23, player24, player25, player26, player27, player28, player29, player30, player31, player32,
+               player33,
+               player34, player35, player36, player37, player38, player39, player40, player41, player42, player43,
+               player44,
+               player45, player46, player47, player48, player49, player50, player51, player52, player53, player54,
+               player55,
+               player56, player57, player58, player59, player60, player61, player62, player63, player64, player65,
+               player66,
+               player67, player68, player69, player70, player71, player72, player73, player74, player75, player76,
+               player77,
+               player78, player79, player80, player81, player82, player83, player84, player85, player86, player87,
+               player88,
+               player89, player90, player91, player92, player93, player94, player95, player96, player97, player98,
+               player99,
+               player100)
+
+    i = 0
+    previous_i = 0
+    print("Use help to open the help menu")
+    while i <= 99:
+        command = input("Enter Command:")
+
+        if command == "help":
+            print("All possible commands:\n"
+                  "help - shows this list\n"
+                  "showlist - shows the list of added names\n"
+                  "add - adds a new name to the list\n"
+                  "rm - removes a name from the list\n")
+        elif command == "showlist":
+            for name in players:
+                if len(name.name) >= 1:
+                    print(players.index(name)+1, name.name)
+                elif len(name.name) == 0 and players.index(name) == 0:
+                    print("The list is empty. Please add names")
+        elif command == "add":
+            if previous_i == 0:
+                name = input("Enter name:")
+                players[i].name = name
+                i += 1
+            else:
+                name = input("Enter name:")
+                players[i].name = name
+                i = previous_i
+        elif command == "rm":
+            index = int(input("Please enter the index number of the name to be removed.\n"
+                              "You can find the indices with 'showlist':"))-1
+            players[index].name = ""
+            previous_i = i
+            i = index
--- main.py
+++ main.py
@@ -2,126 +2,18 @@ try:
     import simplejson as json
 except ImportError:
     import json
-from classes import Player
+from classes import getnames
 
-GRIMLOCK90 = Player("90GRIMLOCK90")
-AHMADGAMD = Player("AHMANDGAMD")
-aki_19_gunner = Player("aki_19_gunner")
-Baconpolse_ = Player("Baconpolse_")
-balazs0354 = Player("balazs0354")
-Balint997 = Player("Balint997")
-banaanekek1 = Player("banaanekek1")
-bardodi = Player("bardodi")
-BaronCupcake = Player("BaronCupcake")
-Base37 = Player("Base37")
-Behe_moth = Player("Behe_moth")
-BlackWolf_29 = Player("BlackWolf_29")
-Blind_n_Spotted = Player("Blind_n_Spotted")
-Boki0065 = Player("Boki0065")
-Bommelman8905 = Player("Bommelman8905")
-bosko_jovanovic_33 = Player("bosko_jovanovic_33")
-BruhMeh = Player("BruhMeh")
-Brunolabadias = Player("Brunolabadias")
-Captain_SmurfarN = Player("Captain_SmurfarN")
-CeeseWar = Player("CeeseWar")
-Ceriously = Player("Ceriously")
-Cheese_Fighter_Man = Player("Cheese_Fighter_Man")
-chicken_nugget_man = Player("chicken_nugget_man")
-Conca = Player("Conca")
-dan_tank_69 = Player("dan_tank_69")
-DaTmaTOX = Player("DaTmaTOX")
-DeadTankKiller = Player("DeadTankKiller")
-Draconic_Luna = Player("Draconic_Luna")
-dragnslyr = Player("dragnslyr")
-Dutch_Duke = Player("Dutch_Duke")
-EdvinE20 = Player("EdvinE20")
-Emrik = Player("Emrik")
-EpicnessLord12 = Player("EpicnessLord12")
-FBSUCHTI_jr = Player("FBSUCHTI_jr")
-fcmf1ishere = Player("fcmf1ishere")
-FDSlash = Player("FDSlash")
-fibi2 = Player("fibi2")
-FrenchMcBaguette = Player("FrenchMcBaguette")
-FusseLofDeath = Player("FusseLofDeath")
-Ghost_7thPzDiv = Player("Ghost_7thPzDiv")
-GhostPlayer22 = Player("GhostPlayer22")
-GrafVonLiechtenstein = Player("GrafVonLiechtenstein")
-Grillosz = Player("Grillosz")
-happy_Sturew = Player("happy_Sturew")
-HydroCannon2016 = Player("HydroCannon2016")
-isse00 = Player("isse00")
-IXI8 = Player("IXI8")
-jagdtiger_1337 = Player("jagdtiger_1337")
-Joe_Winskill = Player("Joe_Winskill")
-jonttu202 = Player("jonttu202")
-KickinPupies = Player("KickinPupies")
-Kreig67 = Player("Kreig67")
-kubamessi2004 = Player("kubamessi2004")
-lars_volvo = Player("lars_volvo")
-Lord_Legend576 = Player("Lord_Legend576")
-LukasK235 = Player("LukasK235")
-mateokg = Player("mateokg")
-Matt_SWE = Player("Matt_SWE")
-MightyMeatMan = Player("MightyMeatMan")
-Mike_Fin = Player("Mike_Fin")
-Miksuli05 = Player("Miksuli05")
-moritzgamer666 = Player("moritzgamer666")
-Mr_Abd000 = Player("Mr_Abd000")
-nancy128 = Player("nancy128")
-nat7020 = Player("nat7020")
-nerdsplay = Player("nerdsplay")
-Nyunya = Player("Nyunya")
-PetrosmanTVGamingPlus = Player("PetrosmanTVGamingPlus")
-petyuka007 = Player("petyuka007")
-Quake_12 = Player("Quake_12")
-RaptierDino = Player("RaptierDino")
-redgaarden = Player("redgaarden")
-Rens64 = Player("Rens64")
-robert_37 = Player("robert_37")
-rollo_lodbrok = Player("rollo_lodbrok")
-Rotz_den_loeffel = Player("Rotz_den_loeffel")
-SERVEJ_cz = Player("SERVEJ_CZ")
-SERVEJ = Player("SERVEJ")
-Shanks_19 = Player("Shanks_19")
-Styleplayer = Player("Styleplayer")
-superhero_2015_gunner = Player("superhero_2015_gunner")
-SuperUnicumNoob_HU = Player("SuperUnicumNoob_HU")
-TankistaMatej368 = Player("TankistaMatej368")
-Tankok1998 = Player("Tankok1998")
-TasmanianDjavoSRB = Player("TasmanianDjavoSRB")
-TGR_Ghost = Player("TGR_Ghost")
-ThatWrapsItUpForYou = Player("ThatWrapsItUpForYou")
-TheDiamondBladeHD = Player("TheDiamondBladeHD")
-THeIrony = Player("TheIrony")
-TheTankMaster2000 = Player("TheTankMaster2000")
-Timberaxe = Player("Timberaxe")
-timohht = Player("timohht")
-TomK2001 = Player("TomK2001")
-Turqoisezomb = Player("Turqoisezomb")
-tyyppi353637 = Player("tyyppi353637")
-VadimNotHacker = Player("VadimNotHacker")
-vojtafren = Player("vojtafren")
-Wilhelmet = Player("Wilhelmet")
-Zwhatsh = Player("Zwhatsh")
 
-Players = (GRIMLOCK90, AHMADGAMD, aki_19_gunner, Baconpolse_, balazs0354, Balint997, banaanekek1, bardodi, BaronCupcake,
-           Base37, Behe_moth, BlackWolf_29, Blind_n_Spotted, Boki0065, Bommelman8905, bosko_jovanovic_33, BruhMeh,
-           Brunolabadias, Captain_SmurfarN, CeeseWar, Ceriously, Cheese_Fighter_Man, chicken_nugget_man, Conca,
-           dan_tank_69, DaTmaTOX, DeadTankKiller, Draconic_Luna, dragnslyr, Dutch_Duke, EdvinE20, Emrik, EpicnessLord12,
-           FBSUCHTI_jr, fcmf1ishere, FDSlash, fibi2, FrenchMcBaguette, FusseLofDeath, Ghost_7thPzDiv, GhostPlayer22,
-           GrafVonLiechtenstein, Grillosz, happy_Sturew, HydroCannon2016, isse00, IXI8, jagdtiger_1337, Joe_Winskill,
-           jonttu202, KickinPupies, Kreig67, kubamessi2004, lars_volvo, Lord_Legend576, LukasK235, mateokg, Matt_SWE,
-           MightyMeatMan, Mike_Fin, Miksuli05, moritzgamer666, Mr_Abd000, nancy128, nat7020, nerdsplay, Nyunya,
-           PetrosmanTVGamingPlus, petyuka007, Quake_12, RaptierDino, redgaarden, Rens64, robert_37, rollo_lodbrok,
-           Rotz_den_loeffel, SERVEJ, SERVEJ_cz, Shanks_19, Styleplayer, superhero_2015_gunner, SuperUnicumNoob_HU,
-           TankistaMatej368, Tankok1998, TasmanianDjavoSRB, TGR_Ghost, ThatWrapsItUpForYou, TheDiamondBladeHD,
-           THeIrony, TheTankMaster2000, Timberaxe, timohht, TomK2001, Turqoisezomb, tyyppi353637, VadimNotHacker,
-           vojtafren, Wilhelmet, Zwhatsh)
+# print("Please enter the complete path to the file with the battles")
+# path_battles = input()
+# battles_data_file = open(path_battles, "r+")
+# battles_data = json.loads(battles_data_file.read())
 
-skirmish_data_file = open("C:/Users/katri/Desktop/Replays/Skirmish210.json", "r+")
-skirmish_data = json.loads(skirmish_data_file.read())
+getnames()
 
-for battle in skirmish_data:
+'''
+for battle in battles_data:
     for index in range(14):
         name = battle["BothTeams"][index]["Name"]
         for player in Players:
@@ -132,3 +24,4 @@ for battle in skirmish_data:
 for player in Players:
     if player.battles >= 1:
         print(player.name, player.battles)
+'''
